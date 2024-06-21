f3e50240d09cc038877626e9dcb4e1cfb6dcaf40
Mory <Mory@gmail.com>
2024-06-21 23:22:09 +0300

    React-Poster-Project

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import PostsList from "./components/PostsList";
+import MainHeader from "./components/MainHeader";
+
+import "./App.css";
+
+const App = () => {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
+  const showModalHandler = () => {
+    setModalIsVisible(true);
+  };
+
+  const hideModalHandler = () => {
+    setModalIsVisible(false);
+  };
+
+  return (
+    <>
+      <MainHeader createPost={showModalHandler} />
+      <main>
+        <PostsList
+          isPosting={modalIsVisible}
+          onStopPosting={hideModalHandler}
+        />
+      </main>
+    </>
+  );
+};
+
+export default App;
